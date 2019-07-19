9f2a55d0e33094fe651fd938ec84983c6b8a354f
thomas-westfall <thomas.westfall64@hunter.cuny.edu>
2019-07-18 21:28:04 -0400

    assignment3 added

---
+++ web-dev/week-one/day-three/solution.js
@@ -0,0 +1,28 @@
+
+//1
+document.getElementById("container");
+
+//2
+document.querySelector("#container.container:first-child");
+
+//3
+document.getElementsByClassName("second");
+
+//4
+const thirdlist = document.getElementsByClassName("third");
+thirdlist[1];
+
+//5
+document.getElementById("container").innerHTML = "Hello!";
+
+//6
+document.getElementsByClassName("footer")[0].classList.add('main');
+
+//7
+document.getElementsByClassName("footer")[0].class
+
+
+// document.open();
+
+// document.write("test");
+// document.close();
