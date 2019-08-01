cc31809025c9cd541b542c892da504f50c43ceab
thomas-westfall <thomas.westfall64@hunter.cuny.edu>
2019-08-01 11:44:16 -0400

    add index.js and action creators

--- web-dev/week-three/day4/counter-reduxify/src/store/index.js
+++ web-dev/week-three/day4/counter-reduxify/src/store/index.js
@@ -20,6 +20,10 @@ import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 // TODO : import reducer functions here - TODO;
+//its done below:
+
+//Reducer functions:
+import counter from "./utilities/counter";
 
 // Construct our Redux store using all of the imports we just conducted;
 // rootReducer: this is the sole reducer function that you will pass to your Redux store when you create the Redux store;
@@ -30,7 +34,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 // store: first argument is your reducer(s) (keep in mind you can just have one reducer without the combineReducers involved****) and your second argument passed in is your middleware that operates in between actions being dispatched and those actions arriving at the reducer function;
 //****; with combineReducers, you are allowed to have split up reducer functions that manage a certain part of state, and this function combineReducers will go through each key in the combinedReducerObject and invoke each reducer function aka value until all reducer functions are called --- however, if you do not use combineReducers you can simply pass in a single reducer function --- so you can pass in an object of reducer functions with combineReducers, or oyu can pass in one sole reducer function without combineReducers;
 // export default store: you can only have one default export per file (unless you alias multiple defaults);
-const rootReducer = combineReducers(/* pass in reducers here later */)
+const rootReducer = combineReducers(counter); //counter: counter
 const logger = createLogger({collapsed: true});
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, logger));
 const store = createStore(rootReducer, middleware);
