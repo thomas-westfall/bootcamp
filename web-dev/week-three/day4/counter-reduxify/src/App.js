69fcd9da1baafa241182558daeef6aa3cdc647ca
thomas-westfall <thomas.westfall64@hunter.cuny.edu>
2019-08-01 11:55:14 -0400

    add app contianer

--- web-dev/week-three/day4/counter-reduxify/src/App.js
+++ web-dev/week-three/day4/counter-reduxify/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { connect } from "react-redux";
+import { increment, decrement} from "./store/utilities/counter";
+
+class AppContainer extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppView counter={this.props.counterr} incrementCounter=
+    {this.props.incrementCounter} decrementCounter={this.props.decrementCounter}/>
   );
 }
+}
 
-export default App;
+// Declaration for mapStateTOProps;
+// The keys in this returned object will be on your component's props object;
+// The values of these keys reflect the value of the piece of state in your redux store;
+const mapState = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+//declaration for mapdispatchtoprops;
+//the keys in this returned object will be on your componet's props object as well;
+// the values of these keys are anonymous functinos that will dispatch imported action creators or thunks so that a component can communicate with the appropriate reducer function(s);
+const mapDispatch = (dispatch) => {
+  return {
+    incrementCounter: () => dispatch(increment()),
+    decrementCounter: () => dispatch(decrement())
+  }
+}
+
+export default connect(mapState, mapDispatch)();
